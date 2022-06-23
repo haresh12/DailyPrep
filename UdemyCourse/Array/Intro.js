eb0016d6832b56b190e8451de5747f6e06aabaf3
Haresh <haresh.lakhwani@mabbly.com>
2022-06-23 08:52:22 +0530

    intro on data structure

---
+++ UdemyCourse/Array/Intro.js
@@ -0,0 +1,35 @@
+// Before we understand array we need to understand what is data structures so in this intro 
+// we are going to talk about that only.
+
+/* Data structures are ways to store the data and in different programing language we have
+   different  data structure to store the data according to our needs or requirement.
+   same thing we do at home like in school bages we store ours books and floder we store
+   our files and in fridge we store fruites and all. And that is because those things are
+   good at storing specific type of things. Same way when we do programing we have different
+   data structure to choose on according to our requirement and needs.
+ */
+
+/*
+  It would be good if we understand how our computer stores the data and also understand 
+  below few words.
+  1. RAM 
+  2. Persistant storage (slower the RAM): 
+  3. Catch 
+
+  These all terms are releated to storage in our computer get more idea from wiki you will love it
+*/
+
+/*
+   Now there are some comman operation that we are going to see in all of the data structure.
+   1. insert : how easliy we can insert data
+   2. delete : how easliy we can delete data
+   3. traversal : looping througt the data
+   4. searching : scan entire data and search for specific data
+   5. sorting 
+   6. Access
+
+   Most of the time above are the 6 parameters that we are going to look for all the problems
+   and will decide which data strucuture to use .
+
+   Common Data Structure Operations : https://www.bigocheatsheet.com  good to look atleast once
+*/
